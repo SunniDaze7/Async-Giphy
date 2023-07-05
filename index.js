647350d1faf7936d195eba03db90f5ef70b03449
sunnidaze7 <pnykii98@gmail.com>
2023-07-05 13:15:14 -0500

    put endpoint within async function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-const endpoint = `https://api.giphy.com/v1/gifs/search??api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+// const endpoint = `https://api.giphy.com/v1/gifs/search??api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
 
 const getImages = async (query)=>{
+   const endpoint = `https://api.giphy.com/v1/gifs/search??api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
    const requestUrl = await fetch(endpoint)
    const data = await requestUrl.json()
    return data
